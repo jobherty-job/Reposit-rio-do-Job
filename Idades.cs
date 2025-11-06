4a9957f26b60cc4ea075f4af53604a146ca8947a
jobherty-job <jobherty.oliveira@aluno.fiema.org.br>
2025-11-06 15:57:25 -0300

    Create Idades.cs

---
+++ Idades.cs
@@ -0,0 +1,21 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{ 
+    string nome1, nome2;
+    int idade1, idade2, m;
+    Console.WriteLine("Digite o nome da primeira pessoa: ");
+    nome1 = Console.ReadLine;
+    Console.WriteLine("Digite o nome da segunda pessoa: ");
+    nome2 = Console.ReadLine;
+    Console.WriteLine("Digite a idade da primeira pessoa: ");
+    idade1 = Console.ReadLine;
+    Console.WriteLine("Digite a idade da segunda pessoa: ");
+    idade2 = Console.ReadLine;
+    m = (idade1+idade2)/2;
+    Console.WriteLine("O nome das pessoas são "+nome1+" e "+nome2+", tem media das idades e igual a "+m);
+  }
+}
+      
